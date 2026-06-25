f543b2c07c3f7f77604ac6a7271f6c5dd72b9bdf
NIWASIIMA ASHELYCOLE <coleniwasiima@gmail.com>
2026-06-26 00:33:07 +0300

    Move app to root directory for foolproof Vercel deployment

--- driveug-app/src/utils/whatsapp.js
+++
@@ -1,6 +0,0 @@
-export const WHATSAPP_NUMBER = "+256767024153";
-
-export function getWhatsAppLink(message) {
-  const encodedMessage = encodeURIComponent(message);
-  return `https://wa.me/${WHATSAPP_NUMBER.replace('+', '')}?text=${encodedMessage}`;
-}
